48b574ff817980e81579f169d3c70743907ba5be
Jacob Zipper <zipper@gatech.edu>
2019-09-02 03:41:02 -0400

    made api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/add/:var1/:var2', function(req, res, next) {
+  res.json({answer: req.params.var1 + req.params.var2});
+});
+
+router.get('/sub/:var1/:var2', function(req, res, next) {
+  res.json({answer: req.params.var1 - req.params.var2});
+});
+
+router.get('/div/:var1/:var2', function(req, res, next) {
+  res.json({answer: req.params.var1 / req.params.var2});
+});
+
+router.get('/mul/:var1/:var2', function(req, res, next) {
+  res.json({answer: req.params.var1 * req.params.var2});
+});
+
 module.exports = router;
